106e250cd6523d3980dd72a7f1e12256ef651aea
Jason Coleman <jason.coleman@gmail.com>
2018-09-06 13:42:46 -0400

    fixed debugging outputt

--- test/process.ps1
+++ test/process.ps1
@@ -4,3 +4,4 @@ Write-Host -Foregroundcolor green "Moving C:\Users\j.coleman\OneDrive\GitHub\FFM
 Move-Item "C:\Users\j.coleman\OneDrive\GitHub\FFMpeg-Batch-Generator\test\mpgfile\file\mpgtest1.mpeg" "\\fs2fast\poolroot\croco\!SortedByResolution\480\mpgfile.mpeg"
 Write-Host -Foregroundcolor Green "Moving C:\Users\j.coleman\OneDrive\GitHub\FFMpeg-Batch-Generator\test\test.mp4 to \\fs2fast\poolroot\croco\!SortedByResolution\480\test.mp4
 Move-Item "C:\Users\j.coleman\OneDrive\GitHub\FFMpeg-Batch-Generator\test\test.mp4" "\\fs2fast\poolroot\croco\!SortedByResolution\480\test.mp4"
+Write-Host -Foregroundcolor green -Message ('Moving ' + .FullName + ' to ' + \\fs2fast\poolroot\croco\!SortedByResolution\ + '480\' +  + '.' + 
