63b8a94b6be6e7d97880912e1425da4d7954db10
Jason Coleman <jason.coleman@gmail.com>
2018-09-25 11:02:51 -0400

    Made remove brackets a funciton.

--- RemoveBrackets.ps1
+++ RemoveBrackets.ps1
@@ -1,15 +1,17 @@
-get-childitem | ForEach-Object {
-    Write-Host "Checking $_"
-    If ($_.Name -Match "\["){
-        Write-Host -ForegroundColor Yellow "Removing left bracket from $_.name"
-        Move-Item -LiteralPath $_.name $_.name.Replace("[","(")
+Function RemoveBrackets(){
+    get-childitem | ForEach-Object {
+        Write-Host "Checking $_"
+        If ($_.Name -Match "\["){
+            Write-Host -ForegroundColor Yellow "Removing left bracket from $_.name"
+            Move-Item -LiteralPath $_.name $_.name.Replace("[","(")
+        }
     }
-}
 
-get-childitem | ForEach-Object {
-    Write-Host "Checking $_"
-    If ($_.Name -Match "\]"){
-        Write-Host -ForegroundColor Yellow "Removing right bracket from $_.name"
-        Move-Item -LiteralPath $_.name $_.name.Replace("]",")")
+    get-childitem | ForEach-Object {
+        Write-Host "Checking $_"
+        If ($_.Name -Match "\]"){
+            Write-Host -ForegroundColor Yellow "Removing right bracket from $_.name"
+            Move-Item -LiteralPath $_.name $_.name.Replace("]",")")
+        }
     }
 }
