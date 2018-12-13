1c1ba0c1c17a626f172a4c48c88ffd68d8160b6e
Jason Coleman <jason.coleman@gmail.com>
2018-12-13 13:37:56 -0500

    Added old AVI cleanup script generation

--- TranscodeAviToMP4.ps1
+++ TranscodeAviToMP4.ps1
@@ -1,4 +1,4 @@
-function Get-AviRemuxBat{
+function Get-AviRemuxScript{
     <# .SYNOPSIS
          Generates a batch file of FFmpeg commands to remux AVI files into MP4 containers
     .DESCRIPTION
@@ -35,23 +35,35 @@ function Get-AviRemuxBat{
     # Trying something a little different, caching the output to memory instead of writing line by line.
 
     $BatBuf = New-Object System.Text.StringBuilder
+    $CleanupBuf = New-Object System.Text.StringBuilder
 
     $FileList = Get-ChildItem -Recurse -Filter '*.avi' -File -ErrorAction 'SilentlyContinue' 
     Foreach ($file IN $FileList){
         # Version of path with mp4 extension:
         $mp4Name = ($file.FullName).TrimEnd(".avi") + ".mp4"
+        $bakName = ($file.FullName).TrimEnd(".avi") + ".bak"
         $BatBuf.Append($mp4Name + "`r`n")
         # Insert the command string
-        $BatBuf.Append("ffmpeg -i '" + $file.FullName + "' -acodec copy -vcodec copy '" + $mp4name + "'`r`n") | Out-Null
+        $BatBuf.Append("ffmpeg -i """ + $file.FullName + """ -acodec copy -vcodec copy """ + $mp4name + """`r`n") 
+        $BatBuf.Append("Rename-Item  ""$file.FullName""  """ + $bakName + """`r`n") 
+        $CleanupBuf.Append("Delete-Item ""$bakName""`r`n")
     }
 
-    Write-Debug -Message "Writing AviRemux.bat"
+    Write-Debug -Message "Writing AviRemux.ps1"
     Try {
         # Note: Out-File inside the VS.Code powershell env sticks a BOM mark in that doesn't happen in normal powershell
-        $BatBuf.ToString()  | out-file AviRemux.bat -Encoding ascii
+        $BatBuf.ToString()  | out-file AviRemux.ps1 -Encoding ascii
     }
     Catch{
-        Throw "Failed to write AviRemux.bat. Batbuf Size:" + $BatBuf.Length
+        Throw "Failed to write AviRemux.ps1. Batbuf Size:" + $BatBuf.Length
     }
 
+    Write-Debug -Message "Writing AviCleanup.ps1"
+    Try {
+        # Note: Out-File inside the VS.Code powershell env sticks a BOM mark in that doesn't happen in normal powershell
+        $CleanupBuf.ToString()  | out-file CleanupBuf.ps1 -Encoding ascii
+    }
+    Catch{
+        Throw "Failed to write CleanupBuf.ps1. CleanupBuf Size:" + $CleanupBuf.Length
+    }
 }
